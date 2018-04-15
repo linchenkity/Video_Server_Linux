97c0a49cdb42c9a3c46f7efd63b602296c5f6e50
haha_Dashen <jiaqiyao123@163.com>
2018-04-16 02:47:23 +0800

    安装脚本 测试通过

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#
+# Video Encode System Install Script
+# Version 0.2-alpha
+#
 echo -e "\033[1;33m[Video Encode System For Linux] \033[0m"
+echo -e "\033[1;32m[Version: 0.2-Alpha] \033[0m"
+echo -e "\033[1;37m[Build By:haha_Dashen]"
+echo -e "[Follow MIT License]\033[0m"
 echo -e "\033[0;34m+Change YUM Base to 163........\033[0m"
+sleep 2
 time_start=$(date +%s)
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS6-Base-163.repo
@@ -59,23 +67,47 @@ echo -e "\033[0;34m+Install PHP\033[0m"
 sleep 2
 cd /usr/local/src
 rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
-yum -y install php70w php70w-mysql php70w-mbstring php70w-mcrypt php70w-gd php70w-imap php70w-ldap php70w-odbc php70w-pear php70w-xml php70w-xmlrpc php70w-pdo php70w-fpm
+yum -y install php70w php70w-mysql php70w-mbstring php70w-mcrypt php70w-gd php70w-imap php70w-ldap php70w-odbc php70w-pear php70w-xml php70w-xmlrpc php70w-pdo php70w-fpm php70w-devel
+echo -e "\033[0;34m+Install Redis\033[0m"
+sleep 2
+yum -y install redis
+echo -e "\033[0;34m+Install PHP-Redis\033[0m"
+sleep 2
+cd /usr/local/src
+wget http://pecl.php.net/get/redis-4.0.0.tgz
+tar xzvf redis-4.0.0.tgz
+cd redis-4.0.0
+phpize
+./configure
+make
+make install
+cp -rf /home/Video_Server_Linux/install/php.ini /etc/php.ini
 echo -e "\033[0;34m+Config Mysql\033[0m"
 sleep 2
 service mysqld start
-mysql_password = head -200 /dev/urandom | cksum | cut -f1 -d" "
+mysql_password=$(head -200 /dev/urandom | cksum | cut -f1 -d" ")
 /usr/bin/mysqladmin -u root password $mysql_password
 service mysqld restart
 echo -e "\033[0;34m+Start Nginx\033[0m"
 /usr/local/nginx/sbin/nginx
 echo -e "\033[0;34m+Start PHP-FPM\033[0m"
 service php-fpm start
+echo -e "\033[0;34m+Start Redis\033[0m"
+service redis start
 echo -e "\033[0;34m+Disable Iptables\033[0m"
 service iptables stop
 echo -e "\033[0;34m+Import Database\033[0m"
+cd /home/Video_Server_Linux/install
 mysql -uroot -p$mysql_password -e "CREATE DATABASE video_server"
-mysql -uroot -p$mysql_password < database_v0.2-alpha.sql
+mysql -uroot -p$mysql_password video_server < database_v0.2-alpha.sql
+echo -e "\033[1;32m"
+echo -e "#####################################"
+echo -e "#                                   #"
+echo -e "#        Video Encode System        #"
+echo -e "#                                   #"
+echo -e "#####################################"
+echo -e "\033[0m"
 time_end=$(date +%s)
 total_time=$(($time_end - time_start))
-echo -e "\033[1;33m Mysql Password:$mysql_password\033[0m";
-echo -e "\033[1;33m Done! $total_time Sec Used\033[0m";
+echo -e "\033[1;33mMysql Password:$mysql_password\033[0m";
+echo -e "\033[1;33mDone! $total_time Seconds Used\033[0m";
