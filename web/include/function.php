03b268d13241cdb2d7ba1778e7d38d5d84e4552e
haha_Dashen <jiaqiyao123@163.com>
2018-04-21 19:57:56 +0800

    修复CPU使用率获取

--- web/include/function.php
+++ web/include/function.php
@@ -70,15 +70,15 @@ function Login_Status(){
 }
 //获取系统负载
 function get_used_status(){
-    $fp = popen('top -b -n 1 | grep -E "^(Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
+    $fp = popen('top -b -n 2 | grep -E "^(Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
     $rs = "";
     while(!feof($fp)){
         $rs .= fread($fp,1024);
     }
     pclose($fp);
     $sys_info = explode("\n",$rs);
-    $cpu_info = explode(",",$sys_info[1]); //CPU占有量 数组
-    $mem_info = explode(",",$sys_info[2]); //内存占有量 数组
+    $cpu_info = explode(",",$sys_info[4]); //CPU占有量 数组
+    $mem_info = explode(",",$sys_info[5]); //内存占有量 数组
 //CPU占有量
     $cpu_usage = trim(trim($cpu_info[0],'Cpu(s): '),'%us'); //百分比
 
