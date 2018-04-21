9b5b7879981913e09f1c5434bf19d581b4a2986d
haha_Dashen <jiaqiyao123@163.com>
2018-04-21 19:44:40 +0800

    性能显示适配Linux

--- web/include/function.php
+++ web/include/function.php
@@ -69,73 +69,25 @@ function Login_Status(){
     }
 }
 //获取系统负载
-class SystemInfoWindows
-{
-    /**
-     * 判断指定路径下指定文件是否存在，如不存在则创建
-     * @param string $fileName 文件名
-     * @param string $content 文件内容
-     * @return string 返回文件路径
-     */
-    private function getFilePath($fileName, $content)
-    {
-        $path = dirname(__FILE__) . "\\$fileName";
-        if (!file_exists($path)) {
-            file_put_contents($path, $content);
-        }
-        return $path;
+function get_used_status(){
+    $fp = popen('top -b -n 1 | grep -E "^(Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
+    $rs = "";
+    while(!feof($fp)){
+        $rs .= fread($fp,1024);
     }
-    /**
-     * 获得cpu使用率vbs文件生成函数
-     * @return string 返回vbs文件路径
-     */
-    private function getCupUsageVbsPath()
-    {
-        return $this->getFilePath(
-            'cpu_usage.vbs',
-            "On Error Resume Next
-    Set objProc = GetObject(\"winmgmts:\\\\.\\root\cimv2:win32_processor='cpu0'\")
-    WScript.Echo(objProc.LoadPercentage)"
-        );
-    }
-    /**
-     * 获得总内存及可用物理内存JSON vbs文件生成函数
-     * @return string 返回vbs文件路径
-     */
-    private function getMemoryUsageVbsPath()
-    {
-        return $this->getFilePath(
-            'memory_usage.vbs',
-            "On Error Resume Next
-    Set objWMI = GetObject(\"winmgmts:\\\\.\\root\cimv2\")
-    Set colOS = objWMI.InstancesOf(\"Win32_OperatingSystem\")
-    For Each objOS in colOS
-     Wscript.Echo(\"{\"\"TotalVisibleMemorySize\"\":\" & objOS.TotalVisibleMemorySize & \",\"\"FreePhysicalMemory\"\":\" & objOS.FreePhysicalMemory & \"}\")
-    Next"
-        );
-    }
-    /**
-     * 获得CPU使用率
-     * @return Number
-     */
-    public function getCpuUsage()
-    {
-        $path = $this->getCupUsageVbsPath();
-        exec("cscript -nologo $path", $usage);
-        return $usage[0];
-    }
-    /**
-     * 获得内存使用率数组
-     * @return array
-     */
-    public function getMemoryUsage()
-    {
-        $path = $this->getMemoryUsageVbsPath();
-        exec("cscript -nologo $path", $usage);
-        $memory = json_decode($usage[0], true);
-        $memory['usage'] = Round((($memory['TotalVisibleMemorySize'] - $memory['FreePhysicalMemory']) / $memory['TotalVisibleMemorySize']) * 100);
-        return $memory;
-    }
+    pclose($fp);
+    $sys_info = explode("\n",$rs);
+    $cpu_info = explode(",",$sys_info[1]); //CPU占有量 数组
+    $mem_info = explode(",",$sys_info[2]); //内存占有量 数组
+//CPU占有量
+    $cpu_usage = trim(trim($cpu_info[0],'Cpu(s): '),'%us'); //百分比
+
+//内存占有量
+    $mem_total = trim(trim($mem_info[0],'Mem: '),'k total');
+    $mem_used = trim($mem_info[1],'k used');
+    $mem_usage = round(100*intval($mem_used)/intval($mem_total),2); //百分比
+
+    return array('cpu_usage'=>$cpu_usage,'mem_usage'=>$mem_usage);
 }
 //API鉴权
 function API_Auth($get,$post){
