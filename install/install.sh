b277023b49e101321b80a87f6276222cb2c45935
linchenkity <51584563+linchenkity@users.noreply.github.com>
2019-06-10 15:44:48 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,7 +11,7 @@ echo -e "\033[0;34m+Change YUM Base to 163........\033[0m"
 sleep 2
 time_start=$(date +%s)
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS6-Base-163.repo
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
 yum clean all
 yum makecache
 echo -e "\033[0;34m+Initialization System........\033[0m"
